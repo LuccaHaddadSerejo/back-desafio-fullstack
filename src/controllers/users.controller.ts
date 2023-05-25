23ef0dd13c97dec5f2fc44d078c54c19e85154c7
LuccaHaddadSerejo <hslucca@gmail.com>
2023-05-25 17:40:30 -0300

    feat: create controllers for users, contacts and login

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import { createUserService } from '../services/Users/createUser.service';
+import { listUsersService } from '../services/Users/listUsers.service';
+import { TUserUpdateRequest } from '../interfaces/Users.interfaces';
+import { updateUserService } from '../services/Users/updateUser.service';
+import { deleteUserService } from '../services/Users/deleteUser.service';
+
+const createUserController = async (req: Request, res: Response) => {
+  const userid = res.locals.userId;
+
+  const newUser = await createUserService(req.body, userid);
+
+  return res.status(201).json(newUser);
+};
+
+const listUsersController = async (req: Request, res: Response) => {
+  const userid = res.locals.userId;
+  const Users = await listUsersService(userid);
+
+  return res.json(Users);
+};
+
+const updateUserController = async (req: Request, res: Response) => {
+  const UserId = req.params.id;
+  const updatedValues: TUserUpdateRequest = req.body;
+  const updateUser = await updateUserService(updatedValues, UserId);
+  return res.json(updateUser);
+};
+
+const deleteUserController = async (req: Request, res: Response) => {
+  const UserId = req.params.id;
+  await deleteUserService(UserId);
+  res.status(204).send();
+};
+
+export {
+  createUserController,
+  listUsersController,
+  updateUserController,
+  deleteUserController,
+};
